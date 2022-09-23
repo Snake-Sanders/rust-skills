56ca976cf4a1b310587e403a0ae1692edf3658d2
Snake Sanders <elmarian22@gmail.com>
2022-09-23 23:14:48 +0200

    generating random numbers

--- the-rust-programming-language/projects/guessing_game/src/main.rs
+++ the-rust-programming-language/projects/guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
+
+    let secret = rand::thread_rng().gen_range(1..=10);
+
+    println!("The secret number is: {secret}.");
     println!("Please input your guess.");
 
     let mut guess = String::new();
