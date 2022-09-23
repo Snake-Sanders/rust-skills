7b82ac62b6137c8397a15fb73b5cb3b75bb5b384
Snake Sanders <elmarian22@gmail.com>
2022-09-23 22:47:30 +0200

    added chapter 1 comments

--- the-rust-programming-language/projects/hello_cargo/src/main.rs
+++ the-rust-programming-language/projects/hello_cargo/src/main.rs
@@ -1,3 +1,18 @@
+// build with:
+// $ cargo build
+//
+// run with:
+// $ ./target/debug/hello_cargo 
+//
+// build&run with:
+// $ cargo run
+//
+// compile without generating executable
+// $ cargo check
+//
+// compile for release 
+// $ cargo build --release
+
 fn main() {
     println!("Hello, world!");
 }
