d23abe013c319900452835892a8e3529788c5631
Snake Sanders <elmarian22@gmail.com>
2022-09-24 06:39:11 +0200

    comparing numbers

--- the-rust-programming-language/projects/guessing_game/src/main.rs
+++ the-rust-programming-language/projects/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -15,5 +16,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // `guess` is shadowing the previous defined variable with the same name
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
