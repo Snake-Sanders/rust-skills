9e001bb4882a6d7ac03211e35f2c3c40a9e06b7f
Snake Sanders <elmarian22@gmail.com>
2022-09-24 07:26:35 +0200

    mutability and shadowing example

--- the-rust-programming-language/projects/variables/src/main.rs
+++ the-rust-programming-language/projects/variables/src/main.rs
@@ -1,3 +1,22 @@
+// it is not allowed to mutate a variable's type
+// let mut spaces = "   ";
+// spaces = spaces.len(); <-- compile error
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is {x}");
+
+    x = 6;
+    println!("The value of x is {x}");
+
+    // new x shadows old x
+    let x = x + 1;
+
+    {
+        // this new x will be disposed at the end of this block
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
