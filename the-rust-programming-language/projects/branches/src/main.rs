e6ba024b5ec2453275f550dd87521cfdeb2600dc
Snake Sanders <elmarian22@gmail.com>
2022-09-25 15:33:56 +0200

    added else if

--- the-rust-programming-language/projects/branches/src/main.rs
+++ the-rust-programming-language/projects/branches/src/main.rs
@@ -6,4 +6,16 @@ fn main() {
     } else {
         println!("condition was false");
     }
+
+    let counter = 6;
+
+    if counter % 4 == 0 {
+        println!("counter is divisible by 4");
+    } else if counter % 3 == 0 {
+        println!("counter is divisible by 3");
+    } else if counter % 2 == 0 {
+        println!("counter is divisible by 2");
+    } else {
+        println!("counter is not divisible by 4, 3, or 2");
+    }
 }
