5ec6717ba824e33bc0486da304505685115fe84d
Snake Sanders <elmarian22@gmail.com>
2022-09-25 15:24:54 +0200

    basic fn return value

--- the-rust-programming-language/projects/functions/src/main.rs
+++ the-rust-programming-language/projects/functions/src/main.rs
@@ -15,10 +15,16 @@ fn five() -> i32 {
     5
 }
 
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
 fn main() {
     print_labeled_measurement(5, 'h');
     let x = five();
-    println!("the value of x is: {x}")
+    let y = plus_one(5);
+    println!("the value of x is: {x}");
+    println!("the value of y is: {y}");
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
