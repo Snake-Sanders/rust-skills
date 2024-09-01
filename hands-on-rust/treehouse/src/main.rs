87476d94b81e8033b7ab427901a28b651a073027
Snake Sanders <elmarian22@gmail.com>
2024-09-01 16:39:15 +0200

    extracted function

--- hands-on-rust/treehouse/src/main.rs
+++ hands-on-rust/treehouse/src/main.rs
@@ -1,8 +1,13 @@
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what's your name?");
+fn what_is_your_name() -> String {
     let mut name = String::new();
     stdin().read_line(&mut name).expect("Failed to read line");
+    return name;
+}
+
+fn main() {
+    println!("Hello, what's your name?");
+    let name = what_is_your_name();
     println!("Hello {}", name);
 }
