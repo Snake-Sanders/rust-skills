5aed771061c1ba6749bbbe477632c7917793f184
Snake Sanders <elmarian22@gmail.com>
2024-12-24 19:25:14 +0100

    add variables to test the linker

--- embedonomicon/lm3s6965/app/src/main.rs
+++ embedonomicon/lm3s6965/app/src/main.rs
@@ -5,8 +5,14 @@ use rt::entry;
 
 entry!(main);
 
+static RODATA: &[u8] = b"hello world";
+static mut BSS: u8 = 0;
+static mut DATA: u16 = 1;
+
 fn main() -> ! {
-    let _x = 42;
+    let _x = RODATA;
+    let _y = &raw const BSS;
+    let _z = &raw const DATA;
 
     loop {}
 }
