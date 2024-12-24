81d12a0173ff2a0744bed8fe48bdd0f430be0a86
Snake Sanders <elmarian22@gmail.com>
2024-12-24 20:19:54 +0100

    initialize memory sections

--- embedonomicon/lm3s6965/rt/src/lib.rs
+++ embedonomicon/lm3s6965/rt/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 use core::panic::PanicInfo;
+use core::ptr;
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
@@ -9,6 +10,26 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
 
 #[no_mangle]
 pub unsafe extern "C" fn ResetHandler() -> ! {
+    // references the linker symbols to
+    // initialize the memory sections
+    extern "C" {
+        static mut _sbss: u8;
+        static mut _ebss: u8;
+        static mut _sdata: u8;
+        static mut _edata: u8;
+        static _sidata: u8;
+    }
+
+    // calculates the size of bss section
+    let count = &raw const _ebss as *const u8 as usize - &raw const _sbss as *const u8 as usize;
+    // set memory to zero
+    ptr::write_bytes(&raw mut _sbss as *mut u8, 0, count);
+
+    // calculates the size of data section
+    let count = &raw const _edata as *const u8 as usize - &raw const _sdata as *const u8 as usize;
+    // initializes the symbols in data
+    ptr::copy_nonoverlapping(&_sidata as *const u8, &raw mut _sdata as *mut u8, count);
+
     extern "Rust" {
         fn main() -> !;
     }
