4648275d92e26b2f9ebbe803dd3e7c279cf32ecf
Snake Sanders <elmarian22@gmail.com>
2024-12-24 23:45:01 +0100

    add sh files for executing debug session

---
+++ embedonomicon/lm3s6965/app/start_dbg.sh
@@ -0,0 +1,3 @@
+echo "starting debugger. Type:"
+echo "target remote :3333"
+arm-none-eabi-gdb -q target/thumbv7m-none-eabi/debug/app
---
+++ embedonomicon/lm3s6965/app/start_qemu.sh
@@ -0,0 +1,8 @@
+echo "starting simulation on port 3333"
+qemu-system-arm \
+      -cpu cortex-m3 \
+      -machine lm3s6965evb \
+      -gdb tcp::3333 \
+      -S \
+      -nographic \
+      -kernel target/thumbv7m-none-eabi/debug/app
