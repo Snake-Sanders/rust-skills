1ae631a1626a66716719cfc1436223e9ffb59cac
Snake Sanders <elmarian22@gmail.com>
2024-12-28 21:28:47 +0100

    moved embedonomicon to its own repo

--- embedonomicon/lm3s6965/app_log/run_start_qemu.sh
+++
@@ -1,8 +0,0 @@
-echo "starting simulation or use <cargo run --release>"
-
-qemu-system-arm \
-      -cpu cortex-m3 \
-      -machine lm3s6965evb \
-      -nographic \
-      -semihosting-config enable=on,target=native \
-      -kernel target/thumbv7m-none-eabi/debug/logger
