5f5159a39388039ac95a212c91a626fd9fc0a50f
Snake Sanders <elmarian22@gmail.com>
2025-01-28 22:33:32 +0100

    add armstrong number

---
+++ exercism/rust/armstrong_numbers.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let number: u32 = 153;
+    if is_armstrong_number(number) {
+        println!("numer: {} is armstrong ", number);
+    } else {
+        println!("numer: {} is not armstrong", number);
+    }
+}
+
+pub fn is_armstrong_number(num: u32) -> bool {
+    let str_num = num.to_string();
+    let digits_count: u32 = str_num.len() as u32;
+    let mut acc: u32 = 0;
+
+    for c in str_num.chars() {
+        let digit = c.to_digit(10).unwrap();
+        println!("acc {}, digit {}, count {}", acc, digit, digits_count);
+        acc += digit.pow(digits_count);
+    }
+
+    return acc == num;
+}
