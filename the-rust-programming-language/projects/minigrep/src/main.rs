07a5b1e18ebf8c26d5ba5bce37c8fb15ad293087
Snake Sanders <elmarian22@gmail.com>
2025-06-07 11:55:29 +0200

    add minigrep

---
+++ the-rust-programming-language/projects/minigrep/src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|e| {
+        println!("Bad arguments: {e}");
+        process::exit(1);
+    });
+    let content = fs::read_to_string(config.file_path).expect("Ops, file  not found");
+    dbg!(content);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
