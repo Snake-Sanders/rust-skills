c110fd9c30b3d4a7d0a912f22b5eda8856aeabfe
Snake Sanders <elmarian22@gmail.com>
2025-06-08 09:27:54 +0200

    format

--- roomer/src/lib.rs
+++ roomer/src/lib.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
-pub fn run(file_path: String) -> (){
-
+pub fn run(file_path: String) -> () {
     let lines = read_file(file_path);
     for line in lines {
         println!("{}", line)
@@ -10,10 +9,9 @@ pub fn run(file_path: String) -> (){
 
 fn read_file(file_path: String) -> Vec<String> {
     let mut res: Vec<String> = vec![];
-   let content = fs::read_to_string(file_path).expect("could not read the input file");
+    let content = fs::read_to_string(file_path).expect("could not read the input file");
     for line in content.lines() {
-       res.push(line.to_string()); 
+        res.push(line.to_string());
     }
     res
 }
-
--- roomer/src/main.rs
+++ roomer/src/main.rs
@@ -7,10 +7,9 @@ fn main() {
     let params: Vec<String> = env::args().collect();
 
     if params.len() != 2 {
-        println!("Invalid paramas. Passed {}, expected 1", params.len() -1 );
+        println!("Invalid paramas. Passed {}, expected 1", params.len() - 1);
         process::exit(1);
     }
 
     roomer::run(params[1].clone());
-
 }
