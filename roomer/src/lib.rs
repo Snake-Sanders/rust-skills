a00f149cbed7f28403f5f6f8b5c9d0c377abfe8a
Snake Sanders <elmarian22@gmail.com>
2025-06-08 09:27:34 +0200

    extracted fun read_file

--- roomer/src/lib.rs
+++ roomer/src/lib.rs
@@ -2,10 +2,18 @@ use std::fs;
 
 pub fn run(file_path: String) -> (){
 
-    let content = fs::read_to_string(file_path).unwrap();
-
-    for line in content.lines() {
+    let lines = read_file(file_path);
+    for line in lines {
         println!("{}", line)
     }
+}
+
+fn read_file(file_path: String) -> Vec<String> {
+    let mut res: Vec<String> = vec![];
+   let content = fs::read_to_string(file_path).expect("could not read the input file");
+    for line in content.lines() {
+       res.push(line.to_string()); 
+    }
+    res
 }
 
