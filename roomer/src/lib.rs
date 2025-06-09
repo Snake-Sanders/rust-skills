571b043c943ed4f6faac2ea5486403bedde003cd
Snake Sanders <elmarian22@gmail.com>
2025-06-09 10:05:08 +0200

    add find areas

--- roomer/src/lib.rs
+++ roomer/src/lib.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use std::fs;
+use std::ops::Range;
 
 #[derive(Debug, PartialEq)]
 struct Wall {
@@ -10,6 +11,12 @@ impl Wall {
     fn new() -> Wall {
         Wall { start: 0, end: 0 }
     }
+}
+
+#[derive(Debug, PartialEq)]
+struct Area {
+    width: Range<usize>,
+    content: String,
 }
 
 #[derive(Debug)]
@@ -56,14 +63,14 @@ fn display_content(content: &Vec<String>) -> () {
 fn parse_line(line: &str) -> Vec<Room> {
     let mut rooms: Vec<Room> = vec![];
     let _walls = find_horizontal_walls(line);
+    let _areas = find_open_areas(line);
     let room = Room::new();
-
+    dbg!(&room);
     rooms.push(room);
 
     rooms
 }
 
-// let re = Regex::new(r"\+-+\+").unwrap();
 fn find_horizontal_walls(line: &str) -> Vec<Wall> {
     let re = Regex::new(r"\.?\+-+\+").unwrap();
 
@@ -78,31 +85,105 @@ fn find_horizontal_walls(line: &str) -> Vec<Wall> {
     walls
 }
 
+fn find_open_areas(line: &str) -> Vec<Area> {
+    // let re = Regex::new(r"(/|\||\\|\+)[^-].+(/|\||\\|\+)").unwrap();
+    let re = Regex::new(r"(/|\||\\|\+)[^-/\|\\\+]+").unwrap();
+
+    let areas: Vec<_> = re
+        .find_iter(line)
+        .map(|m| {
+            let r = m.range();
+            let width = r.start..r.end ;
+            let content = m.as_str().to_string();
+
+            Area {
+                width: width,
+                content: content,
+            }
+        })
+        .collect();
+
+    areas
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
+    fn find_empty_multi_open_areas() {
+        let line = "  |   | |";
+        let areas = find_open_areas(line);
+        assert_eq!(2, areas.len());
+        assert_eq!(areas[0], Area{content: "|   ".to_string(), width: 2..6});
+        assert_eq!(areas[1], Area{content: "| ".to_string(), width: 6..8});
+
+        let line = " +--+  /  \\  +-- ";
+        let areas = find_open_areas(line);
+        assert_eq!(3, areas.len());
+        assert_eq!(areas[0], Area{content: "+  ".to_string(), width: 4..7});
+        assert_eq!(areas[1], Area{content: "/  ".to_string(), width: 7..10});
+        assert_eq!(areas[2], Area{content: "\\  ".to_string(), width: 10..13});
+    }
+
+    #[test]
+    // #[ignore = "alrady tested"]
+    fn find_empty_open_areas() {
+        let line = "|   |";
+        let areas = find_open_areas(line);
+        assert_eq!(1, areas.len());
+        assert_eq!(
+            areas[0],
+            Area {
+                content: "|   ".to_string(),
+                width: 0..4
+            }
+        );
+
+        let line = "+  \\";
+        let areas = find_open_areas(line);
+        assert_eq!(1, areas.len());
+        assert_eq!(
+            areas[0],
+            Area {
+                content: "+  ".to_string(),
+                width: 0..3
+            }
+        );
+
+        let line = " /  +--";
+        let areas = find_open_areas(line);
+        assert_eq!(1, areas.len());
+        assert_eq!(
+            areas[0],
+            Area {
+                content: "/  ".to_string(),
+                width: 1..4
+            }
+        );
+    }
+    #[test]
+    //#[ignore = "alrady tested"]
     fn find_invalid_start_h_wall() {
-        let content = " -+";
-        let walls = find_horizontal_walls(content);
+        let line = " -+";
+        let walls = find_horizontal_walls(line);
         assert_eq!(0, walls.len());
 
-        let content = " ++ ";
-        let walls = find_horizontal_walls(content);
+        let line = " ++ ";
+        let walls = find_horizontal_walls(line);
         assert_eq!(0, walls.len());
     }
     #[test]
     fn find_two_valid_start_h_wall() {
-        let content = " +-+ +---+ ";
-        let walls = find_horizontal_walls(content);
+        let line = " +-+ +---+ ";
+        let walls = find_horizontal_walls(line);
         assert_eq!(2, walls.len());
         assert_eq!(Wall { start: 1, end: 3 }, walls[0]);
         assert_eq!(Wall { start: 5, end: 9 }, walls[1]);
     }
     #[test]
     fn find_one_valid_start_h_wall() {
-        let content = "+--+";
-        let walls = find_horizontal_walls(content);
+        let line = "+--+";
+        let walls = find_horizontal_walls(line);
         assert_eq!(1, walls.len());
         assert_eq!(Wall { start: 0, end: 3 }, walls[0]);
     }
